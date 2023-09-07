18b1043366a434d6166c68a5e9e45d51f979a384
Adyrnney <dineifb@yahoo.com.br>
2023-09-07 15:15:10 +0200

    Lambda expression as argument

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,12 +16,8 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 450.0));
 		
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
-			
-		
-		list.sort(comp);
+		list.sort((p1, p2) -> 
+		 p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
 		for(Product p : list) {
 			System.out.println(p);
